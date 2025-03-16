c28d1e7ea45023c5eb403d0d1989e558bea0044e
Ernesto <ernesto.privato@gmail.com>
2025-03-16 23:50:49 +0100

    fix curl

--- api/flights.js
+++ api/flights.js
@@ -17,11 +17,23 @@ module.exports = async (req, res) => {
         mfFlightType: 'P'
       },
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'keyId': '6bc034ea-ae66-40ce-891e-3dccf63cb2eb',
         'Accept': '*/*',
+        'Accept-Language': 'en,it-IT;q=0.9,it;q=0.8,en-US;q=0.7',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'DNT': '1',
         'Origin': airport === 'mxp' ? 'https://www.milanomalpensa-airport.com' : 'https://www.milanolinate-airport.com',
-        'Referer': airport === 'mxp' ? 'https://www.milanomalpensa-airport.com/' : 'https://www.milanolinate-airport.com/'
+        'Pragma': 'no-cache',
+        'Referer': airport === 'mxp' ? 'https://www.milanomalpensa-airport.com/' : 'https://www.milanolinate-airport.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'cross-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0',
+        'keyId': '6bc034ea-ae66-40ce-891e-3dccf63cb2eb',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"'
       }
     });
 
